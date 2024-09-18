ad006d84e4c6f371dc57f2e9e3b449dc481f7be8
TierraCodes <112350897+TierraCodes@users.noreply.github.com>
2024-09-18 15:42:22 -0400

    initial changes

---
+++ app.js
@@ -0,0 +1,9 @@
+let container = document.createElement("div")
+let squaresDiv = document.getElementsByClassName("squares")
+
+container.setAttribute();
+
+document.body.appendChild(container)
+
+container.classList.add('squares')
+
