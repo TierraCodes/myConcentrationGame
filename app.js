856b8f1921c6dcfe6280a1f7e6c8471635a84ae2
TierraCodes <112350897+TierraCodes@users.noreply.github.com>
2024-09-27 23:50:25 -0400

    Made it work
    I met with Dr. Faas and he showed me what I was missing to actually make the game work!

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
-let dataSecret = document.getElementsByClassName("squares")
+const squares = document.getElementsByClassName("squares")
+const matchesLeft = document.getElementById("matchesLeft")
 
-const value = dataSecret.getValue;
+let num = squares.length;
 
-
+let card1 = null
+let card2 = null
+matchesLeft.value = 8
 
 function getValue(){
-    dataSecret.addEventListener("click", (event) =>{
-        let dataValue = dataSecret.value;
-        if (dataValue == dataSecret){
-            return(
-                dataSecret.innerHTML = value
-            )
-        }
-    })
-}
 
+}
+for(let i=0; i<squares.length;i++){
+    squares[i].addEventListener("click", (event) =>{
+        event.target.innerHTML = event.target.dataset.secret
+        if (card1 != null){
+            card2 = event.target
+            if (checkMatch()){
+                card1.remove()
+                card2.remove()
+                card1 = null
+                card2 = null
+                matchesLeft.value -=1
+                matchesLeft.innerHTML = "Matches Left: " + matchesLeft.value
+            } else {
+                setTimeout(() =>{
+                    event.target.innerHTML = ""
+                    card1.innerHTML = ""
+                    card2.innerHTML = ""
+                    card1 = null
+                    card2 = null
+                }, 1000)
+            }
+        } else {
+            card1 = event.target
+        }
+
+    })
+    
+}
+function checkMatch(){
+    console.log (card1)
+    return (card1.dataset.secret == card2.dataset.secret)
+}
